a2f041ccb4c12a366cb039adafe6b89107867880
Miguel <miguel@gmail.com>
2026-02-11 11:16:02 +0100

    Controlar división entre cero en método dividir

--- src/calculadora15/Calculadora15.java
+++ src/calculadora15/Calculadora15.java
@@ -15,8 +15,11 @@ public class Calculadora15 {
 	public int multiplicar(int a, int b) { return a * b;
 	}
 
-	public double dividir(int a, int b) { return (double) a / b;
-	}
+	public double dividir(int a, int b) { if (b == 0) {
+		throw new IllegalArgumentException("No se puede dividir entre cero");
+		}
+		return (double) a / b;
+		}
 
 }
 
