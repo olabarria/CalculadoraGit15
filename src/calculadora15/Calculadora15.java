9131aaaebe63893de23b4e47fd8bddf675458480
Miguel <miguel@gmail.com>
2026-02-11 11:05:43 +0100

    Proyecto CalculadoraGIT15 clase Calculadora y método restar

--- src/calculadora15/Calculadora15.java
+++ src/calculadora15/Calculadora15.java
@@ -2,13 +2,13 @@ package calculadora15;
 
 public class Calculadora15 {
 
-	public int sumar(int a, int b) {
+	public static int sumar(int a, int b) {
 		return a+b;
 		
 
 	}
 
-	public int restar(int a, int b) {
+	public static int restar(int a, int b) {
 		 return a - b;
 		 }
 }
