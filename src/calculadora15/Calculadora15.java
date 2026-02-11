b6942e092d53eb3876d591a38c8767b25b1c3295
Miguel <miguel@gmail.com>
2026-02-11 20:27:43 +0100

    Actualizo Calculadora15

--- src/calculadora15/Calculadora15.java
+++ src/calculadora15/Calculadora15.java
@@ -2,33 +2,43 @@ package calculadora15;
 
 public class Calculadora15 {
 
-	public static int sumar(int a, int b) {
-		return a+b;
-		
+    
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
 
-	}
+    
+    public static int restar(int a, int b) {
+        return a - b;
+    }
 
-	public static int restar(int a, int b) {
-		 return a - b;
-		 }
-	
-	public int multiplicar(int a, int b) { return a * b;
-	}
+    
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
 
-	public double dividir(int a, int b) { if (b == 0) {
-		throw new IllegalArgumentException("No se puede dividir entre cero");
-		}
-		return (double) a / b;
-		}
-	public int potencia(int base, int exponente) { int resultado = 1;
-	for (int i = 0; i < exponente; i++) { resultado *= base;
-	}
-	return resultado;
-	}
+    
+    public static double dividir(int a, int b) {
+        if (b == 0) {
+            System.out.println("Error: No se puede dividir entre cero");
+            return 0; 
+        }
+        return (double) a / b;
+    }
 
-	public int modulo(int a, int b) { return a % b;
-	}
+    
+    public static int potencia(int base, int exponente) {
+        int resultado = 1;
+        for (int i = 0; i < exponente; i++) {
+            resultado *= base;
+        }
+        return resultado;
+    }
 
-
+    
+    public static int modulo(int a, int b) {
+        return a % b;
+    }
 }
+
 
